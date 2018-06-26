eeda93abd7efd46e92ffc262242478ee12ade99a
David Michaux <michaux.dr@gmail.com>
2018-06-26 13:19:06 -0400

    Add #new and #create actions

--- app/controllers/recipient_controller.rb
+++ app/controllers/recipient_controller.rb
@@ -1,2 +1,23 @@
 class RecipientController < ApplicationController
+
+	def new
+		@recipient = Recipient.new
+	end
+
+	def create
+		@recipient = Recipient.new(recipient_params)
+		if @recipient.save
+			@recipient.send_card
+			flash[:notice] = "Card sent to #{@recipient.name}"
+			redirect_to root_path
+		else
+			render :new
+		end
+	end
+
+	private
+
+	def recipient_params
+		params.require(:recipient).permit(:name, :email, :phone)
+	end
 end
