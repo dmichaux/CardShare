513ac832cfbb8645b2858ee5b01b1cd915df8f8f
David Michaux <michaux.dr@gmail.com>
2018-06-26 17:00:22 -0400

    Add Gravatar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,6 +4,13 @@ module ApplicationHelper
     time.localtime.strftime('%A, %-m/%-d/%Y %I:%M %p')
   end
 
+  # Returns a Gravatar image for given user
+  def gravatar_for(user)
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+  end
+
 	# Helpers for Devise forms
   def resource_name
     :user
--- app/mailers/recipient_mailer.rb
+++ app/mailers/recipient_mailer.rb
@@ -1,5 +1,7 @@
 class RecipientMailer < ApplicationMailer
 
+	add_template_helper(ApplicationHelper)
+
   def card(recipient, current_user)
   	@recipient 		= recipient
   	@current_user = current_user
