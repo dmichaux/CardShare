10d5623a7cf5632c3320ebec84d618ee5612b598
David Michaux <michaux.dr@gmail.com>
2018-06-27 11:39:22 -0400

    Move add_template_helper to application_mailer.rb

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,7 @@
 class ApplicationMailer < ActionMailer::Base
+  
+	add_template_helper(ApplicationHelper)
+
   default from: 'noreply@CardShare.com'
   layout 'mailer'
 end
--- app/mailers/recipient_mailer.rb
+++ app/mailers/recipient_mailer.rb
@@ -1,7 +1,5 @@
 class RecipientMailer < ApplicationMailer
 
-	add_template_helper(ApplicationHelper)
-
   def card(recipient, current_user)
   	@recipient 		= recipient
   	@current_user = current_user
