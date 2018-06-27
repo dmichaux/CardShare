d991ceba36f68024922dde67efa7a7be75859c56
David Michaux <michaux.dr@gmail.com>
2018-06-27 11:38:46 -0400

    Update preview method to match

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -3,7 +3,9 @@ class UserMailerPreview < ActionMailer::Preview
 
   # Preview this email at http://localhost:3000/rails/mailers/user_mailer/confirmation
   def confirmation
-    UserMailer.confirmation
+  	current_user = User.first
+  	recipient 	 = Recipient.first
+    UserMailer.confirmation(current_user, recipient)
   end
 
 end
