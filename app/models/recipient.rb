7512f87dbeb2f962127ab72def286770c99d9951
David Michaux <michaux.dr@gmail.com>
2018-06-27 12:56:34 -0400

    Remove uniqueness validation for email

--- app/models/recipient.rb
+++ app/models/recipient.rb
@@ -7,8 +7,7 @@ class Recipient < ApplicationRecord
 	validates :name, presence: true
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 	validates :email, presence: true, length: { within: 5..254 },
-										format: { with: VALID_EMAIL_REGEX },
-										uniqueness: { case_sensitive: false}
+										format: { with: VALID_EMAIL_REGEX }
 
 	def send_card(current_user)
 		RecipientMailer.card(self, current_user).deliver_now
